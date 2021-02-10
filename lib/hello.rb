f5b39dd6b055e5d7276714151b704ef5f1a97cdf
Kevin <kevinarogers1@gmail.com>
2021-02-10 07:11:26 -0500

    commit tree b8706afe7366fe48742e9843a90fcfc03f1f16c2

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,22 +1,24 @@
 def hello_t(array)
+  if block_given?
     i = 0
 
     while i < array.length
-      yield array [i]
+      yield(array[i])
       i = i + 1
     end
+
     array
   else
-  else 
     puts "Hey! No block was given!"
-end
-end
-
+  end
+end 
+    
 
  # call your method here!
  hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
+  if name.start_with?("T")
+    puts "Hi, #{name}"
   end
+end 
+ 
 
